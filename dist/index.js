79ec89779e627ff47f69beff461c0c91f4fb7f38
Flurin <mail@flurin.de>
2021-01-24 11:34:56 +0100

    added .env file

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const multer = require("multer");
+const env = require("dotenv");
+env.config();
 const upload = multer();
 // Initialize express and define a port
 const app = express();
-const PORT = 3000;
 app.post('/event', upload.none(), (req, res) => {
     console.log(req.body); // Call your action on the request here
+    const bodyJson = JSON.parse(req.body);
+    console.log(bodyJson);
     res.status(200).end();
 });
 // Start express on the defined port
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`🚀 Server running on port ${process.env.PORT}`));
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express = require('express')
 import multer = require('multer')
+import env = require('dotenv')
+env.config()
+
 const upload = multer()
 // Initialize express and define a port
 const app = express()
-const PORT = 3000
+
 
 app.post('/event', upload.none(), (req, res) => {
   console.log(req.body) // Call your action on the request here
+  const bodyJson = JSON.parse(req.body)
+  console.log(bodyJson)
   res.status(200).end()
 })
 
 // Start express on the defined port
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+app.listen(process.env.PORT, () => console.log(`🚀 Server running on port ${process.env.PORT}`))
